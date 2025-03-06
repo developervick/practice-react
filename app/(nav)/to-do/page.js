a8ea9a4071fad259353dcb32c6eea0af91af9f3c
developervick <developervick@gmail.com>
2025-03-06 23:09:23 +0530

    created side bar

---
+++ app/(nav)/to-do/page.js
@@ -0,0 +1,14 @@
+
+
+
+function ToDoPage() {
+  return (
+    <>
+      <div>
+        To Do List
+      </div> 
+    </>
+  );
+}
+
+export default ToDoPage;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    content:['./app/**/*.js', './node_modules/preline/preline.js',],
+    plugins: [
+    ],
+}
